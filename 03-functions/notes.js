1508bef37193b54c5f56c99d1a6e9f49bf27b56c
IvanMollov <ivan.xero.mollov@gmail.com>
2018-02-22 11:59:39 +0200

    Updates 03-function notes.

--- 03-functions/notes.js
+++ 03-functions/notes.js
@@ -12,18 +12,12 @@ console.log(a);
 console.log(b);
 console.log(c);
 
-// NOTE: every var in js is just a reference
-//       this means that when you assign a something to var 
-//       this just changes where the var points to
-(function (a) 
-{
-    a.b = 13;
-    
-    a = {
-        a: 3,
-        b: 13
-    };
-})(a);
+// NOTE: functions can be called before they are defined
+f(a);
+
+// NOTE: fc is not a function
+//       it is a reference to a function and must be defined before it is used
+fc(a);
 
 // NOTE: #3 if the let is changed the const is changed also
 b.a = 13;
@@ -31,3 +25,30 @@ b.a = 13;
 console.log(a);
 console.log(b);
 console.log(c);
+
+// NOTE: default arguments
+// NOTE: if a function is called with less arguments then defined
+//       they are assigned undefined
+// NOTE: if more - they are ignored
+// NOTE: every var in js is just a reference
+//       this means that when you assign a something to var 
+//       this just changes where the var points to
+// NOTE: functions create a closure
+//       vars there are created every time the function is called
+// NOTE: best is when a function does only a single thing
+function f(a, arg1 = 2, c) 
+{
+    // NOTE: a is local (not the var from upper scope) now
+
+    // NOTE: functions see vars from upper scopes
+    console.log(b);
+
+    a.b = 13;
+    
+    a = {
+        a: 3,
+        b: 13
+    };
+}
+
+let fc = f;
